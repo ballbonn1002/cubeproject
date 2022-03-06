54b1fc461932aacba92dcc398a5378cc59a6b995
ballbonn1002 <84431637+ballbonn1002@users.noreply.github.com>
2022-03-06 23:40:13 +0700

    PROD / UAT

--- ROOT/src/sql/dbscript.sql
+++ ROOT/src/sql/dbscript.sql
@@ -21,8 +21,31 @@ CREATE TABLE `user_rpw`(
    `user_rpw_id` INT(11) NOT NULL AUTO_INCREMENT,
    `user_id` VARCHAR(32) NOT NULL,
    `user_key` VARCHAR(32) NOT NULL,
-   `expired` timestamp NOT NULL,      
+   `expired` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),      
    PRIMARY KEY (`user_rpw_id`)
+);
+
+CREATE TABLE `ticket` (
+  `ticket_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ticket_name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `user_create` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `time_create` timestamp NULL DEFAULT current_timestamp(),
+  `ticket_url` varchar(120) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `expected_result` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `actual_result` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `file_id` bigint(20) DEFAULT NULL,
+  `ticket_status_id` int(11) DEFAULT NULL,
+  `user_assigned` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `time_assigned` timestamp NULL DEFAULT NULL,
+  `time_success` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`ticket_id`)
+);
+
+CREATE TABLE `ticket_status` (
+  `ticket_status_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ticket_status_name` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`ticket_status_id`)
 );
 
 INSERT INTO `role_authorized_object`(`role_id`, `authorized_object_id`) VALUES 
@@ -32,3 +55,17 @@ INSERT INTO `role_authorized_object`(`role_id`, `authorized_object_id`) VALUES
 ('ACCOUNTING', 'dashboard.view'),
 ('AE', 'dashboard.view'),
 ('AE2', 'dashboard.view');
+
+ALTER TABLE `timesheet` ADD `project` VARCHAR(32) NULL AFTER `id`;
+
+ALTER TABLE `timesheet` ADD `summary` VARCHAR(32) NULL AFTER `project`;
+
+ALTER TABLE `timesheet` ADD `started_date` DATE NULL AFTER `description`;
+
+ALTER TABLE `timesheet` ADD `timespent` VARCHAR(32) NULL AFTER `started_date`;
+
+-- PROD / UAT -- [5 Mar 2022
+]
+
+
+
